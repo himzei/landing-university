2039057e730d9cee1a053d232d94ad636a632a74
조현일 <himzei@gmail.com>
2023-10-16 20:32:42 +0900

    re

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/leaflet",
+        path: "leaflet",
         element: <Leaflet />,
       },
     ],
