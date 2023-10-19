d575e637feaf8108da7a9d868f7b5cdd5c245321
조현일 <himzei@gmail.com>
2023-10-20 07:23:10 +0900

    re

--- src/routes/Curriculumn.jsx
+++ src/routes/Curriculumn.jsx
@@ -1,3 +1,14 @@
+import LayoutSub from "../components/LayoutSub";
+import Section from "../components/Section";
+
 export default function Curriculumn() {
-  return <div></div>;
+  return (
+    <LayoutSub>
+      <Section>
+        <div className="text-3xl font-semibold text-red-500 flex justify-center py-32">
+          준비중
+        </div>
+      </Section>
+    </LayoutSub>
+  );
 }
